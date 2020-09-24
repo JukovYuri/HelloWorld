a85a7b11834efadc00cdfcee6a05e23dd1b1587a
jukov_yuti <--->
2020-09-24 21:09:46 +0300

    занятие 3

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public int damage;
+
+    void Start()
+    {
+        print($"Hello, I'm {name}");
+    }
+
+
+    public void Attack(int damageParam)
+    {
+
+        health -= damageParam;
+        print($"Health life {health}");
+        if (health <= 0)
+           {
+                print($"{name} dead");
+           }
+    }
+
+    public int GetHealth()
+        {
+            return health;
+        }
+
+     void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/MagicNumbers.cs
+++ Assets/Scripts/MagicNumbers.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class MagicNumbers : MonoBehaviour
 {
+	public Text title;
 	public int min = 1;
 	public int max = 1000;
 	int guess;
@@ -11,6 +14,8 @@ public class MagicNumbers : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
 	{
+		title.text = "Magic Numbers";
+		title.color = Color.cyan;
 		//min = 1;
 		//max = 1000;
 		print($"загадайте число от {min} до {max}");
